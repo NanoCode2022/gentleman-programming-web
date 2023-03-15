a4af69876a23978b7e2d340d92e8acba1e8ac4b7
Jovany <jovanyevs@gmail.com>
2023-03-14 20:56:08 -0600

    change sidebar

---
+++ app/Twitch/page.tsx
@@ -0,0 +1,5 @@
+function Twitch() {
+  return <div>Youtube - 120</div>;
+}
+
+export default Twitch;
--- shared/ui/layouts/sidebar/sidebar.tsx
+++ shared/ui/layouts/sidebar/sidebar.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
 
 import styles from './sidebar.module.scss';
-const Sidenav: FC = () => (
-  <aside className={styles.root}>
-    <p>sidebar</p>
-  </aside>
-);
 
+const Sidenav: FC = () => {
+  return (
+    <aside className={styles.root}>
+      <div className={styles['content-img']}>
+        <img src="/img/l-n.png" alt="" className={styles.img} />
+        <img src="/img/t-n.png" alt="" className={styles.img} />
+        <img src="/img/d-n.png" alt="" className={styles.img} />
+        <img src="/img/Y-n.png" alt="" className={styles.img} />
+        <img src="/img/m-n.png" alt="" className={styles.img} />
+        <div className={styles.select}></div>
+      </div>
+    </aside>
+  );
+};
 export default Sidenav;
